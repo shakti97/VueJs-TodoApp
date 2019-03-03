bb0789d25a3a6db4ddd6e6e1802f0ec949591252
shakti97 <shaktisingh230797@gmail.com>
2019-03-03 21:08:48 +0530

    Build TodoInput Component

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class='todoInput'>
+        <input type="text" class='form-control' @input='task' placeholder='Enter the new Task'>
+        <button class="btn btn-primary addTask" @click='addTask'>Submit</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'TodoInput',
+    data(){
+        return {
+            taskItem : ''
+        }
+    },
+    methods : {
+        task(event){
+            this.taskItem = event.target.value;
+        },
+        addTask(){
+            this.$emit('addTask',this.taskItem);
+        }
+    }
+}
+</script>
+
+<style>
+    .addTask{
+        margin-top : 2%;
+        width : 20%;
+    }
+</style>
+
+

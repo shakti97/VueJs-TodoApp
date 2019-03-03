2bcee2deb1b9e638c24fb93631dd7efc5218ec21
shakti97 <shaktisingh230797@gmail.com>
2019-03-03 21:07:56 +0530

    Build App Container

---
+++ src/components/TodoContent.vue
@@ -0,0 +1,51 @@
+<template>
+        <div class="todoContent">
+            <TodoInput @addTask="addTask"/>
+            <TodoList :todos="todos"  @removeItem="removeItem"/>
+        </div>
+</template>
+
+<script>
+import TodoInput from  './TodoInput.vue';
+import TodoList from "./TodoList.vue";
+
+export default {
+    name : 'TodoContent',
+    data(){
+        return{
+            todoItem : 1,
+            task : 'No task',
+            todos : [],
+            removingElement : ''
+        }
+    },
+    components : {
+        TodoInput,
+        TodoList
+    },
+    methods : {
+        addTask(e){
+            this.task=e;
+            var todoObject={
+                id : this.todoItem,
+                title : this.task
+            }
+            
+            this.todos.push(todoObject);
+            this.todoItem++;
+        },
+        removeItem(e){
+            this.removingElement=e;
+        }
+    }
+
+}
+</script>
+
+<style >
+    .todoContent{
+        margin: 2% 20%;
+    }
+</style>
+
+

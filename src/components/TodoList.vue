91048efa644cd01231ff6f1b964b4494b511d6ed
shakti97 <shaktisingh230797@gmail.com>
2019-03-03 21:09:21 +0530

    Build TodoList Component

---
+++ src/components/TodoList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class='container'>
+        <div v-for="todo in todos" :key="todo.id">
+            <TodoItem :todo="todo" @removeItem="removeItem"/>
+        </div>
+    </div>    
+</template>
+
+<script>
+import TodoItem from "./TodoItem.vue";
+export default {
+    props : ["todos"],
+    name : 'TodoList',
+    components : {
+        TodoItem
+    },
+    methods : {
+        removeItem(e){
+            this.$emit('removeItem',e);
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>

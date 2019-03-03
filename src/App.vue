3293004bb50fd28b35c6ea013ec88197bcbafe77
shakti97 <shaktisingh230797@gmail.com>
2019-03-03 12:51:42 +0530

    Build Header:- first component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import Header from './components/Header.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Header
   }
 }
 </script>
@@ -23,6 +24,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="header">
+        <div><img alt="Vue logo" width='100px' height='100px' src="../assets/logo.png">
+        <span class='heading'>TODO APP</span></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'Header',
+    props : {
+        header_name : String
+    }
+}
+</script>
+
+<style scoped>
+.header{
+    padding: 5%;
+    background-color: rgb(33, 33, 63);
+}
+.heading{
+    color: rgb(165, 161, 161);
+    font-size : 60px;
+    font-weight: 800;
+    vertical-align: middle;
+}
+</style>
